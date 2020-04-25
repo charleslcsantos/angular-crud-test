e6fe97c395c6b2701b9030c1d89659a3a4e454fe
Charles <charleslcsantos@gmail.com>
2020-04-25 20:25:17 -0300

    added crud methods

--- src/app/Modules/user/user.component.ts
+++ src/app/Modules/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService, IUser } from './user.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  public users: Observable<IUser[]> = null;
+  public user: IUser;
 
-  constructor() { }
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.users = this.userService.getAll();
+  }
+
+  async save(user?: IUser) {
+    if (!user) {
+      user = this.user;
+    }
+
+    await this.userService.save(user).toPromise();
+    this.getUsers();
   }
 
 }
---
+++ src/app/Modules/user/user.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpService } from 'src/app/services/HttpService/http.service';
+import { Observable } from 'rxjs';
+
+export interface IUser {
+  avatar: string;
+  email: string;
+  id?: number;
+  name: string;
+  phone: string;
+  username: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  endpoint = 'users';
+
+  constructor(private httpService: HttpService) { }
+
+  getAll(): Observable<IUser[]> {
+    return this.httpService.get(this.endpoint);
+  }
+
+  save(user: IUser) {
+    if (user.id) {
+      return this.httpService.put(`${this.endpoint}/${user.id}`, user);
+    } else {
+      return this.httpService.post(this.endpoint, user);
+    }
+  }
+
+  remove(user) {
+    this.httpService.delete(`${this.endpoint}/${user.id}`);
+  }
+}
