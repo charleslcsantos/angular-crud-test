42af0db9454c765dc41c0236cecfd79480d280eb
Charles <charleslcsantos@gmail.com>
2020-04-27 20:39:15 -0300

    added icon components

--- src/app/shared/icons/icon.module.ts
+++ src/app/shared/icons/icon.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { IconEditComponent } from "./icon-edit/icon-edit.component";
 import { IconRemoveComponent } from "./icon-remove/icon-remove.component";
+import { IconArrowComponent } from "./icon-arrow/icon-arrow.component";
 
-const COMPONENTS = [IconEditComponent, IconRemoveComponent];
+const COMPONENTS = [IconEditComponent, IconRemoveComponent, IconArrowComponent];
 
 @NgModule({
   declarations: [COMPONENTS],
